d74cb2d619d29fdd22ed1270385cbdde5e13e315
nelzord <jmn3lson@gmail.com>
2021-04-26 12:41:53 -0700

    included basic map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation'
-
+import SimpleMap from './components/SimpleMap'
 function App() {
   return (
     <div className="App">
       <Navigation></Navigation>
+      <SimpleMap></SimpleMap>
     </div>
   );
 }
