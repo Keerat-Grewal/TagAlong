dfd2774755f6e890b983b2e862f4e56ebc1858b0
Keerat_Grewal <kgrewa03@calpoly.edu>
2021-04-28 15:04:57 -0700

    added import

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,18 +1,19 @@
 //add firebase - did yarn add firebase
 import firebase from 'firebase/app'
-import "firebase/auth"
+import "firebase/auth";
+import 'firebase/firestore';
 
 
 const app = firebase.initializeApp({
    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-   storageBucket: proces.env.REACT_APP_FIREBASE_STORE_BUCKET,
+   storageBucket: process.env.REACT_APP_FIREBASE_STORE_BUCKET,
    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
    appId: process.env.REACT_APP_FIREBASE_APP_ID
  })
 
 
 export const auth = app.auth();
-export const firestore = firebase.firestore();
+export const firestore = app.firestore();
 export default app;
