17b3cbbec6a8d23e674e22f1acd86a4e1dbedea1
Brandon Burana <bburana@calpoly.edu>
2021-04-28 13:21:11 -0700

    Aligned map on the left

--- src/components/SimpleMap.js
+++ src/components/SimpleMap.js
@@ -4,9 +4,10 @@ class SimpleMap extends Component {
   
  
   render() {
-    return (  
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d101400.99868591237!2d-122.09555218029577!3d37.418645988136866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1619465478782!5m2!1sen!2sus" width="600" height="450" style={{marginRight:600}} allowfullscreen="" loading="lazy"></iframe>
-      
+    return (
+        <div class="SimpleMapContainer">  
+          <iframe class="SimpleMapIframe" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d101400.99868591237!2d-122.09555218029577!3d37.418645988136866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1619465478782!5m2!1sen!2sus" allowfullscreen="" loading="lazy"></iframe>
+        </div>
     );
   }
 }
