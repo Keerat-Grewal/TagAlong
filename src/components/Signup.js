9cebfdab67a1a415f1fed9ce1158bf8656f67093
Ishaan Karvir <ikarvir@calpoly.edu>
2021-04-29 04:43:50 +0530

    Working account creation

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,33 @@
-import React, {useRef} from 'react'
-import {Card, Button, Form, FormControl, Container} from 'react-bootstrap'
 
+import React, {useRef, useState} from 'react'
+import {Alert, Card, Button, Form, FormControl, Container} from 'react-bootstrap'
+import { useAuth } from '../contexts/AuthContext'
 
 export default function Signup(){
    const emailRef = useRef();
    const passwordRef = useRef();
    const passwordConfirmRef = useRef();
+   const {signup } = useAuth();
+   const [error, setError] = useState('');
+   const [loading, setLoading] = useState(false);
+
+   async function handleSubmit(e){
+      e.preventDefault()
+      if(passwordRef.current.value !== passwordConfirmRef.current.value){
+         return setError("Passwords do not match");
+      }  
+      
+      try{
+         setError("")
+         setLoading(true)
+         await signup(emailRef.current.value, passwordRef.current.value)
+      } catch(error){
+         console.log(error);
+         setError("Failed to create an account")
+      }
+      setLoading(false)
+   }
+
    return (
 
       <Container className = "d-flex align-items-center justify-content-center"
@@ -14,11 +36,12 @@ export default function Signup(){
          <Card>
             <Card.Body>
                <h2 className = "text-center mb-4">Sign Up</h2>
-               <Form>
+               {error && <Alert variant = "danger"> {error} </Alert>}
+               <Form onSubmit={handleSubmit}>
                   <Form.Group id="email">
+                  </Form.Group>
                      <Form.Label>Email</Form.Label>
                      <Form.Control type = "email" ref={emailRef} required></Form.Control>
-                  </Form.Group>
                   <Form.Group id="Password">
                      <Form.Label>Password</Form.Label>
                      <Form.Control type = "password" ref={passwordRef} required></Form.Control>
@@ -27,7 +50,7 @@ export default function Signup(){
                      <Form.Label>Password Confirmation</Form.Label>
                      <Form.Control type = "password" ref={passwordConfirmRef} required></Form.Control>
                   </Form.Group>
-                  <Button className = "w-100" type="submit">Sign up</Button>
+                  <Button disabled = {loading} className = "w-100" type="submit">Sign up</Button>
                </Form>
             </Card.Body>
          </Card>
