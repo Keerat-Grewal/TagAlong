8c35c8c1f3d205a88a490c594953f5e552d084fb
Ishaan Karvir <ikarvir@calpoly.edu>
2021-04-30 01:46:15 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -5,12 +5,26 @@ import SimpleMap from './components/SimpleMap'
 import { Container } from 'react-bootstrap';
 import Signup from './components/Signup';
 import { AuthProvider } from './contexts/AuthContext';
+import {BrowserRouter as Router, Switch,  Route} from 'react-router-dom'
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute'
+
+
 function App() {
   return (
+     <Router>
      <AuthProvider>
-   <Signup></Signup>
-   </AuthProvider>
+        <Switch>
+           <Route path = "/signup" component = {Signup}></Route>
+           <Route path = "/login" component = {Login}></Route>
+           <PrivateRoute exact path = "/" component = {Navigation}></PrivateRoute>
+         </Switch>
+        </AuthProvider>
+      </Router>
 
+
+   // {/* <Signup></Signup> */}
+   
    //  <div className="App">
    //    <Navigation></Navigation>
    //    <SimpleMap></SimpleMap>
