033c7fbdf1430e4c746e8c043297a9bf760fd1c5
nelzord <jmn3lson@gmail.com>
2021-05-02 17:41:00 -0700

    switched to ol3 api

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navigation from './Navigation'
 import SimpleMap from './SimpleMap'
 import Chat from './Chat'
+import OLMapFragment from './SimpleMap'
 
 export default function MainContent() {
    return (
     <div className="App">
+      
       <Navigation></Navigation>
-      <SimpleMap></SimpleMap>
+      <OLMapFragment></OLMapFragment>
       <Chat></Chat>
     </div>
    )
