11a57fc3c9cc525ef52ef4b7c3f85c3b5dec083d
Keerat_Grewal <kgrewa03@calpoly.edu>
2021-05-04 23:53:29 -0700

    added file to get current location of user

---
+++ src/components/Location.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+
+const useGeoLocation = () => {
+    const [location, setLocation] = useState({
+        loaded: false,
+        coordinates: { lat: "", lng: "" },
+    });
+
+    const changePosition = (location) => {
+        setLocation({
+            loaded: true,
+            coordinates: {
+                lat: location.coords.latitude,
+                lng: location.coords.longitude,
+            },
+        });
+    };
+
+    console.log("IN POSITION");
+    useEffect(() => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(changePosition);
+        }
+
+    }, []);
+
+    return location;
+};
+
+export default useGeoLocation;
