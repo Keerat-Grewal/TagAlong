e62927342ace57551f1fa15b294f19dce0a3792f
nelzord <jmn3lson@gmail.com>
2021-05-21 17:15:25 -0700

    added testing interface

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
+import { Navbar } from 'react-bootstrap';
 import App from './App';
+import Navigation from './components/Navigation';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+
+
+// });
+
+it('basic test', () => {
+   const linkElement = 1
+   expect(linkElement).toBeValid()
+   })
