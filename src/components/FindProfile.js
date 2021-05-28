a3ae848d3353c3bee91c7c5b6f0f0edc7cf0264e
Keerat_Grewal <kgrewa03@calpoly.edu>
2021-05-28 02:04:27 -0700

    added find profile feature

---
+++ src/components/FindProfile.js
@@ -0,0 +1,123 @@
+import React, {useState, useEffect} from 'react';
+import {Button, Form, Col, Modal, Image, Row, Container} from 'react-bootstrap';
+import Avatar from '../avatar3_clear.png';
+import { useAuth } from '../contexts/AuthContext';
+import {firestore, storage} from './Firebase';
+
+export default function FindProfile() {
+
+    const {signup, currentUser} = useAuth();
+    const [showModal, setModal] = useState(false);
+    const [showUser, setUser] = useState(false);
+    const [showError, setError] = useState(false);
+    const [userInfo, setUserInfo] = useState('');
+    const [formValue, setFormValue] = useState({username: ""});
+    const [profilePicture, setProfilePicture] = useState(undefined);
+    const [profilePictureFlag, setProfilePictureFlag] = useState(false);
+
+    const handleHide = () => {
+        setModal(false);
+    }
+
+    const handleChange = (e) => {
+        setFormValue({ 
+            ...formValue, 
+            [e.target.id]: e.target.value
+        });
+    }
+
+    const handleClick = () => {
+        //console.log(formValue.username);
+        firestore.collection("users").where("username", "==", formValue.username)
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    // doc.data() is never undefined for query doc snapshots
+                    setUser(true);
+                    setUserInfo(doc.data());
+                });
+            })
+            .catch((error) => {
+                console.log("Error getting documents: ", error);
+                setUser(false);
+            });
+    }
+
+
+    useEffect(() => {
+        // usersRef.get().then((doc) => {
+        //    if (doc.exists) {
+        //       setBio(doc.data().bio)
+        //       setFirstName(doc.data().firstName)
+        //       setLastName(doc.data().lastName)
+        //       setUserName(doc.data().username)
+        //       storage.ref('pictures').child(doc.data().ProfilePicture).getDownloadURL().then((temp) => {setProfilePicture(temp);
+        //                                                    setPreviewPic(temp)})
+        //       setProfilePictureFlag(true)
+        //       // setProfilePicture(b)
+        //       // setPreviewPic(b)
+        //    } 
+        //    else {
+        //       // doc.data() will be undefined in this case
+        //       console.log("No such document! useEffect");
+        //    }
+        // }).catch((error) => {
+        //       console.log("Error getting document:", error);
+        //    });
+        if(userInfo !== '' && setUser) {
+            storage.ref('pictures').child(userInfo.ProfilePicture).getDownloadURL().then((temp) => {setProfilePicture(temp);});
+            setProfilePictureFlag(true);
+        }
+     }, [userInfo])
+
+    return (
+        <>
+            <Button id="ride-btn" style={{marginLeft: 10, background: "#E84F11", border: "#E84F11"}} onClick={() => setModal(true)}>
+                Find Profile<Image style={{height: "25px", width: "35px"}} src={Avatar}></Image>
+            </Button>
+            <Modal
+                show={showModal}
+                onHide={handleHide}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header onClick={handleHide} closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Find Profile
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Group>
+                        <Form.Row>
+                            <Col xs={11}>
+                                <Form.Control id="username" placeholder="Enter username" onChange={handleChange}></Form.Control>
+                            </Col>
+                            
+                            <Col xs={1}>
+                                <Button style={{background: "#E84F11", border: "#E84F11"}} type="submit" id="destination" onClick={handleClick}>&#10003;</Button>
+                            </Col>
+                        </Form.Row>
+                    </Form.Group>
+                </Modal.Body>
+                {showUser && profilePictureFlag && 
+                    <Container fluid>
+                        <Row>
+                            <Col xs={6}>
+                                <Image style={{marginLeft: "10px", marginBottom: "10px", height: "350px", width: "350px"}} roundedCircle fluid src={profilePicture}></Image>
+                            </Col>
+                            <Col xs={6}>
+                                <h2 style={{fontFamily: "Verdana"}}>{userInfo.firstName + " " + userInfo.lastName}</h2>
+                                <h2 style={{fontFamily: "Verdana", color: "#E84F11", fontSize: "18pt"}}>{"@" + userInfo.username}</h2>
+                                <h2 style={{fontFamily: "Verdana", marginTop: "40px", wordWrap: "break-word", fontSize: "12pt"}}>{userInfo.bio}</h2>
+                            </Col>
+                        </Row>
+                    
+                    </Container>}
+                {/* {!showUser && <p> User does not exist! </p>} */}
+                <Modal.Footer>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,59 +1,16 @@
 
-import React, {useRef, useState, useEffect, useCallback} from 'react';
-import { Row } from 'react-bootstrap';
-import { Col } from 'react-bootstrap';
+import React, {useState, useEffect} from 'react';
 import {Button, Form, FormControl} from 'react-bootstrap';
-import {Navbar, Nav, NavDropdown, Image, NavItem, Modal} from 'react-bootstrap'
-import {useAuth} from '../contexts/AuthContext'
-import Logout from './Logout';
+import {Navbar, Nav, NavDropdown, Image, NavItem, Modal} from 'react-bootstrap';
+import {useAuth} from '../contexts/AuthContext';
 import Logo from '../logo_3.png';
 import '../styles/navigation.css';
-import CreateRide from './Ride'
-import Searchbar from './Searchbar';
+import CreateRide from './Ride';
 import FindRide from './FindRide';
-import NavProfile from './NavProfile'
+import NavProfile from './NavProfile';
+import FindProfile from './FindProfile';
 import Reset from '../reset_button3.png';
-import {Link} from 'react-router-dom'
-
-
-// class Navigation extends Component{
-//     constructor(props){
-//         super(props);
-//         this.state = {filter: ""}
-//     }
-
-//     handle 
-//     render() {
-//         console.log("NAVIGATION")
-//         return(
-//             <Navbar bg="light" expand="lg">
-//             {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
-//             <Navbar.Brand>
-//                 <Image id="logo" src={Logo} />
-//             </Navbar.Brand>
-//             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//             <Navbar.Collapse id="basic-navbar-nav">
-//               {/* <Nav className="mr-auto">
-//                 <Nav.Link href="#home">Home</Nav.Link>
-//                 <Nav.Link href="#link">Link</Nav.Link>
-//               </Nav> */}
-//               {/* <Form inline>
-//                 <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-//                 <Button variant="outline-success">Search</Button>
-//               </Form> */}
-//               <CreateRide></CreateRide>
-//               <FindRide></FindRide>
-//               {/* <Searchbar style= {{margin: "auto"}}></Searchbar> */}
-//             </Navbar.Collapse>
-//             {/* <Logout></Logout> */}
-//             <NavProfile/>
-//           </Navbar>
-
-//         )
-//     }
-// }
-
-// export default Navigation;
+import {Link} from 'react-router-dom';
 
 
 export default function Navigation(props) {
@@ -90,8 +47,7 @@ export default function Navigation(props) {
           {props.display && <CreateRide></CreateRide>} 
           {props.display && <FindRide update={setFilter}></FindRide>}
           {props.display && <Image id="reset" src={Reset} onClick={handleClick}></Image>}
-    
-          {/* <Searchbar style= {{margin: "auto"}}></Searchbar> */}
+          {props.display && <FindProfile></FindProfile>}
         </Navbar.Collapse>
         {/* <Logout></Logout> */}
         <NavProfile/>
