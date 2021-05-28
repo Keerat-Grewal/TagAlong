39d5321ef0e2a93c8c4462b50fc572eea8107c82
Keerat_Grewal <kgrewa03@calpoly.edu>
2021-05-28 02:13:20 -0700

    added home link to navbar on profile page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -36,10 +36,10 @@ export default function Navigation(props) {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          {/* <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-          </Nav> */}
+          {!props.display && 
+            <Nav className="mr-auto">
+              <Nav.Link style={{color: "#E84F11", border: "#E84F11", fontFamily: "Verdana"}} href="/">Home</Nav.Link>
+            </Nav>}
           {/* <Form inline>
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
             <Button variant="outline-success">Search</Button>
