a9171fff902ca1e7a35dc2f39aeee406b54561b0
Ishaan Karvir <ikarvir@calpoly.edu>
2021-05-30 12:17:13 +0530

    Removed simple map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation'
-import SimpleMap from './components/SimpleMap'
 import { Container } from 'react-bootstrap';
 import Signup from './components/Signup';
 import { AuthProvider } from './contexts/AuthContext';
